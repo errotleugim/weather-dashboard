0109621176e90da5a3d37523d64b808cd07973a9
errotleugim <94770602+errotleugim@users.noreply.github.com>
2022-01-22 20:14:51 -0800

    Added local storage of last city. still need to make a loop to do it 5 times. fml

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-var input = document.querySelector('.input_text');
+var inpuut = document.querySelector('.input_text');
 var main = document.querySelector('#name');
 var temp = document.querySelector('.temp');
 var desc = document.querySelector('.desc');
 var clouds = document.querySelector('.clouds');
 var button= document.querySelector('.submit');
 
+
 button.addEventListener('click', function(){
-  fetch('http://api.openweathermap.org/geo/1.0/direct?q='+input.value+'&limit=5&appid=2db2aad8f9cf5856066bd821773ec276')
+  fetch('http://api.openweathermap.org/geo/1.0/direct?q='+inpuut.value+'&limit=5&appid=2db2aad8f9cf5856066bd821773ec276')
   .then(response => response.json())
   .then(data => {
     var latitude = data[0]['lat'];
@@ -15,7 +16,9 @@ button.addEventListener('click', function(){
     console.log(longitude);
     localStorage.setItem('lat', latitude);
     localStorage.setItem('lon', longitude);
+    
   })
+  .then(localStorage.setItem('City1', inpuut.value))
   .then (fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+localStorage.getItem('lat')+'&lon='+localStorage.getItem('lon')+'&exclude={part}&appid=2db2aad8f9cf5856066bd821773ec276')
   .then(response => response.json())
   .then(data => {
@@ -29,7 +32,7 @@ button.addEventListener('click', function(){
     // input.value ="";
   console.log(data);
   })
-  
+
   .catch(err => alert("Wrong city name!")))
   })
 

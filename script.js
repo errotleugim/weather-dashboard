29a2e9cc0a31dbbabcdf6a562579fba522a2bb2a
errotleugim <94770602+errotleugim@users.noreply.github.com>
2022-01-22 19:28:35 -0800

    Added geolocation API.

--- script.js
+++ script.js
@@ -5,9 +5,18 @@ var desc = document.querySelector('.desc');
 var clouds = document.querySelector('.clouds');
 var button= document.querySelector('.submit');
 
-
-button.addEventListener('click', function(name){
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=2db2aad8f9cf5856066bd821773ec276')
+button.addEventListener('click', function(){
+  fetch('http://api.openweathermap.org/geo/1.0/direct?q='+input.value+'&limit=5&appid=2db2aad8f9cf5856066bd821773ec276')
+  .then(response => response.json())
+  .then(data => {
+    var latitude = data[0]['lat'];
+    var longitude = data[0]['lon'];
+    console.log(latitude);
+    console.log(longitude);
+  })
+})
+button.addEventListener('click', function(){
+fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+latitude+'&lon='+longitude+'&exclude={part}&appid=2db2aad8f9cf5856066bd821773ec276')
 .then(response => response.json())
 .then(data => {
   var tempValue = data['main']['temp'];

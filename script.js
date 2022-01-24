5f76c6c47c365aaa69da800a564089e8c9d244b9
errotleugim <94770602+errotleugim@users.noreply.github.com>
2022-01-23 21:37:46 -0800

    Started on the 5 day forecast. Confused about everything. Sick of screens.

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ var clouds = document.querySelector('.clouds');
 var button= document.querySelector('.submit');
 var wind = document.querySelector('.wind');
 var hum = document.querySelector('.hum');
-var fivedays = document.querySelector('.fivedays');
+var fivedays = document.querySelector('#fivedays');
+var fecha = document.querySelector('#fecha');
 var API_KEY = '2db2aad8f9cf5856066bd821773ec276'
-data = '';
+;
 lastsearch();
 
 function lastsearch () {
@@ -38,6 +39,10 @@ function today () {
     var windvalue = data['current']['wind_speed']
     var tempvalue = data['current']['temp']
     var humvalue = data['current']['humidity']
+    const date = moment().format('YYYY-MM-DD');
+    
+    fecha.innerHTML = date;
+    main.innerHTML = inpuut.value ;
     wind.innerHTML = "Wind speed: " + windvalue + " KM/H";
     temp.innerHTML = "Temperature: " + tempvalue + " C°";
     uvi.innerHTML = "UV Index: "+uvivalue;
@@ -47,37 +52,80 @@ function today () {
 }
 function fivedaysweather () {
   fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+localStorage.getItem('lat')+'&lon='+localStorage.getItem('lon')+'&exclude={part}&appid='+API_KEY)
-  .then(response => response.json())
+    .then(response => response.json())
   .then(data => {
-    console.log(data);
-     
-})
+  console.log(data['daily'])
+  //Loop it or type it in manually
+  //Maybe try to make an array or object with the first five elements
+  //and only the elements i need.
+          fivedays.innerHTML = `
+         
+          <div class="col-2 day1"><h4>Tomorrow</h4>
+          <p>Temperature</p>
+          <p>Wind</p>
+          <p>Humidity</p>
+        </div>
+        <div class="col-2 day2"><h4>Day After</h4>
+          <p>Temperature</p>
+          <p>Wind</p>
+          <p>Humidity</p>
+        </div>
+        <div class="col-2 day3"><h4>3 days from now</h4>
+          <p>Temperature</p>
+          <p>Wind</p>
+          <p>Humidity</p>
+        </div>
+        <div class="col-2 day4"><h4>4 days from now</h4>
+          <p>Temperature</p>
+          <p>Wind</p>
+          <p>Humidity</p>
+        </div>
+        <div class="col-2 day5"><h4>5 days from now</h4>
+          <p>Temperature</p>
+          <p>Wind</p>
+          <p>Humidity</p>
+        </div>
+             `
+  })
+  //Ideally this would clear the local storage and fix the need to click submit twice
+  //Doesn't work. Maybe call this function some other way? Maybe use session storage?
+  //Rather, find a way to pass the coordinates without using storage.
+  // localStorage.removeItem('lat');
+  // localStorage.removeItem('lon');
 }
+
 button.addEventListener('click', function(){
+  uvcolorclear();
   getlatlon(); 
   today ();
   uvcolor();
   fivedaysweather();
 })
 
-function uvcolor () {
-  if (uvi.innerHTML <= 2 ) {
-    let element = document.getElementById("uvindex");
-    element.classList.add("low");
-  }
-  else if (uvi.innerHTML <= 5) {
-    let element = document.getElementById("uvindex");
-    element.classList.add("medium");
-  }
-  else if (uvi.innerHTML <= 7) {
-    let element = document.getElementById("uvindex");
-    element.classList.add("high");
-  }
-  else if (uvi.innerHTML <= 10) {
-    let element = document.getElementById("uvindex");
-    element.classList.add("veryhigh");
-  } 
+function uvcolorclear() {
+  let element = document.getElementById("uvindex");
+  element.classList.remove("low","high","veryhigh", "medium");
 }
+function uvcolor() {
+  if (uvi.innerHTML > 0 && uvi.innerHTML < 2 ) {
+    let element = document.getElementById("uvindex");
+        element.classList.add("low");
+  }
+  else if (uvi.innerHTML > 2 && uvi.innerHTML < 5) {
+    let element = document.getElementById("uvindex");
+        element.classList.add("medium");
+  }
+  else if (uvi.innerHTML < 7 && uvi.innerHTML > 10) {
+    let element = document.getElementById("uvindex");
+        element.classList.add("high");
+  }
+  else {
+    let element = document.getElementById("uvindex");
+        element.classList.add("veryhigh");
+   };
+  
+  } ;
+
 
   // .then(data => {
   //   var uvivalue = data['current']['uvi']
